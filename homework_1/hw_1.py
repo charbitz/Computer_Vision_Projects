73bb9b28041835ed0011805c2ed198661eab0377
charbitz <charbitz20@gmail.com>
2021-11-10 02:27:14 +0200

    Initial commit.

---
+++ homework_1/hw_1.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+
+orig_1_path = 'dataset/1_original.png'
+nois_1_path = 'dataset/1_noise.png'
+
+orig_1 = cv2.imread(orig_1_path, cv2.IMREAD_GRAYSCALE)
+nois_1 = cv2.imread(nois_1_path, cv2.IMREAD_GRAYSCALE)
+
+print(orig_1.shape)
+print(nois_1.shape)
+
+cv2.namedWindow('1_original')
+cv2.imshow('1_original', orig_1)
+cv2.waitKey(0)
+
+cv2.namedWindow('1_noise')
+cv2.imshow('1_noise', nois_1)
+cv2.waitKey(0)
+
+# Linear filtering:
+
+# kernel = np.array([[-8,0,8], [-2,0,2], [-8,0,8]])
+kernel = 1/9*np.ones((3,3))
+
+nois_1_filt = cv2.filter2D(nois_1, cv2.CV_8UC1, kernel)
+
+cv2.namedWindow('1_noise_filtered')
+cv2.imshow('1_noise_filtered', nois_1_filt)
+cv2.waitKey(0)
+
+nois_1_after_median = cv2.medianBlur(nois_1,3)
+
+cv2.namedWindow('nois_1_after_median')
+cv2.imshow('nois_1_after_median', nois_1_after_median)
+cv2.waitKey(0)
+
+# Non linear filtering:
+
+strel_cr = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
+print("strel MORPH_CROSS")
+print(strel_cr)
+
+nois_1_eros = cv2.morphologyEx(nois_1, cv2.MORPH_ERODE, strel_cr)
+cv2.namedWindow('nois_1_eros')
+cv2.imshow('nois_1_eros', nois_1_eros)
+cv2.waitKey(0)
