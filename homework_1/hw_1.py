fc047a9e06100b198f0fdc7435fd86884de7d0cf
charbitz <charbitz20@gmail.com>
2021-11-12 03:32:31 +0200

    Using matplotlib the histogram of the filtered image was computed in order to select the right threshold.

--- homework_1/hw_1.py
+++ homework_1/hw_1.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 orig_1_path = 'dataset/1_original.png'
 nois_1_path = 'dataset/1_noise.png'
@@ -29,9 +30,15 @@ nois_1_after_median_r = cv2.resize(nois_1_after_median, (650, 800))
 cv2.imshow('nois_1_after_median', nois_1_after_median_r)
 cv2.waitKey(0)
 
+# Computing the histogram of the filtered image:
+
+plt.hist(nois_1_after_median.ravel(),256,[0,256])
+plt.title("Histogram of nois_1_after_median")
+plt.show()
+
 # Converting to Binary image:
 
-retval, filt_med_1_bin = cv2.threshold(nois_1_after_median, thresh=200, maxval = 255, type=cv2.THRESH_BINARY )           # a first test of the function
+retval, filt_med_1_bin = cv2.threshold(nois_1_after_median, thresh=215, maxval = 255, type=cv2.THRESH_BINARY)           #  first tests of the function
 
 cv2.namedWindow('filt_med_1_bin')
 filt_med_1_bin_r = cv2.resize(filt_med_1_bin, (650, 800))
