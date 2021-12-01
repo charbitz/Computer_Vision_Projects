67f0005004c95b4874e57a261cadd48d2f39a8b1
charbitz <charbitz20@gmail.com>
2021-12-01 22:13:15 +0200

    Started an implementation for the code to run automatically for all images at once
    Thoughts:
    - may change the sequence (5, 4->3->2->1) to (1, 2->3->4->5) and also the sequence of (image_2, image_1) to (image_1, image_2) and also the sequence (img_pt2, img_pt1) to (img_pt1, img_pt2).
    - may change match2() function, in order to succeed cross checking.

--- homework_2/hw_2.py
+++ homework_2/hw_2.py
@@ -1,81 +1,67 @@
 import numpy as np
 import cv2
 
-# image :
-# image_path = "notre-dame-1.jpg"
-image_path = 'dataset/yard-house/yard-house-05.png'
-# image_path = 'dataset/my-images/png/image_1.png'
-image = cv2.imread(image_path)
+def panorama(image_1, image_2):
 
-cv2.namedWindow('image')
-cv2.imshow('image', image)
-cv2.waitKey(0)
+    # sift for images :
+    sift = cv2.xfeatures2d_SIFT.create(3000)
 
-# image_gr = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-#
-# cv2.namedWindow('image_gr')
-# cv2.imshow('image_gr', image_gr)
-# cv2.waitKey(0)
+    # sift keypoints and descriptors of image :
+    kp1 = sift.detect(image_1)
+    desc1 = sift.compute(image_1, kp1)
 
-# sift for images :
-sift = cv2.xfeatures2d_SIFT.create(2000)
+    # sift keypoints and descriptors of image_2 :
+    kp2 = sift.detect(image_2)
+    desc2 = sift.compute(image_2, kp2)
 
-# sift keypoints and descriptors of image :
-kp1 = sift.detect(image)                 # tosa keypoints, octave
-desc1 = sift.compute(image, kp1)         # desc1[1]: grammes keiypoints , sthles einai ta 128  characteristics toy kaue keypoint
+    # match_2 matches :
+    matches = match2(desc1[1], desc2[1])
 
-# image with keypoints :
-# image_w_kp = cv2.drawKeypoints(image, kp1, image)
-#
-# cv2.namedWindow('image')
-# cv2.imshow('image', image_w_kp)
-# cv2.waitKey(0)
+    image_w_matches = cv2.drawMatches(image_1, desc1[0], image_2, desc2[0], matches, None)
+    cv2.namedWindow('image_w_matches')
+    image_w_matches_r = cv2.resize(image_w_matches, (1300, 800))
+    cv2.imshow('image_w_matches', image_w_matches_r)
+    cv2.waitKey(0)
+    cv2.destroyWindow('image_w_matches')
 
-# image_2 :
-image_path2 = 'dataset/yard-house/yard-house-04.png'
-# image_path2 = "notre-dame-2.jpg"
-image_2 = cv2.imread(image_path2)
+    # implementation for matches_2 :
+    img_pt1 = []
+    img_pt2 = []
+    for x in matches:
+        img_pt1.append(kp1[x.queryIdx].pt)
+        img_pt2.append(kp2[x.trainIdx].pt)
+    img_pt1 = np.array(img_pt1)
+    img_pt2 = np.array(img_pt2)
 
-cv2.namedWindow('image_2')
-cv2.imshow('image_2', image_2)
-cv2.waitKey(0)
+    # homography :
+    M = 0
+    mask = 0
+    M, mask = cv2.findHomography(img_pt2, img_pt1, cv2.RANSAC)
 
-# sift keypoints and descriptors of image_2 :
-kp2 = sift.detect(image_2)
-desc2 = sift.compute(image_2, kp2)
+    # merged image :
+    merged_image = []
+    merged_image = cv2.warpPerspective(image_2, M, (image_1.shape[1] + 1000, image_1.shape[0] + 1000))
 
-# image_2 with keypoints :
-# image_2_w_kp = cv2.drawKeypoints(image_2, kp2, image_2)
-#
-# cv2.namedWindow('image_2')
-# cv2.imshow('image_2', image_2_w_kp)
-# cv2.waitKey(0)
+    cv2.namedWindow('merged_image')
+    merged_image_r = cv2.resize(merged_image, (1300, 800))
+    cv2.imshow('merged_image', merged_image_r)
+    cv2.waitKey(0)
+    cv2.destroyWindow('merged_image')
 
-def match1(d1, d2):
-    n1 = d1.shape[0]
-    n2 = d2.shape[0]
+    merged_image[0: image_1.shape[0], 0: image_1.shape[1]] = image_1
 
-    matches = []
-    for i in range(n1):
-        fv = d1[i, :]
-        diff = d2 - fv
-        diff = np.abs(diff)
-        distances = np.sum(diff, axis=1)
+    # for i in image_1.shape[0]:
+    #     for j in image_1.shape[1]:
+    #         if image_1[i][j] != 0:
+    #             merged_image[i][j] = image_1[i][j]
 
-        i2 = np.argmin(distances)
-        mindist2 = distances[i2]
+    cv2.namedWindow('merged_image_2')
+    merged_image_r = cv2.resize(merged_image, (1300, 800))
+    cv2.imshow('merged_image_2', merged_image_r)
+    cv2.waitKey(0)
+    cv2.destroyWindow('merged_image_2')
 
-        matches.append(cv2.DMatch(i, i2, mindist2))
-
-    return matches
-
-# match_1 matches :
-matches_1 = match1(desc1[1], desc2[1])
-
-image_match_1 = cv2.drawMatches(image, desc1[0], image_2, desc2[0], matches_1, None)
-cv2.namedWindow('match1')
-cv2.imshow('match1', image_match_1)
-cv2.waitKey(0)
+    return merged_image
 
 def match2(d1, d2):
     n1 = d1.shape[0]
@@ -101,45 +87,63 @@ def match2(d1, d2):
 
     return matches
 
-# match_2 matches :
-matches_2 = match2(desc1[1], desc2[1])
+def cropping(image):
 
-image_match_2 = cv2.drawMatches(image, desc1[0], image_2, desc2[0], matches_2, None)
-cv2.namedWindow('match2')
-cv2.imshow('match2', image_match_2)
-cv2.waitKey(0)
+    image_gr = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # cv2.namedWindow('image_gr')
+    # image_gr_r = cv2.resize(image_gr, (1300, 800))
+    # cv2.imshow('image_gr', image_gr_r)
+    # cv2.waitKey(0)
 
-# brute force matches :
-bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)              # συνηθως βαζω την NORM_L1 επιεδη ειναι πιο γρήγορη , το crosscheck κανει απο την μια προσ την αλλη και απο την αλλη προς την μια
-bf_matches = bf.match(desc1[1], desc2[1])
+    thresh, image_bin = cv2.threshold(src=image_gr, thresh=0, maxval=255, type=cv2.THRESH_BINARY)
+    # cv2.namedWindow('image_bin')
+    # image_bin_r = cv2.resize(image_bin, (1300, 800))
+    # cv2.imshow('image_bin', image_bin_r)
+    # cv2.waitKey(0)
 
-image_match_bf = cv2.drawMatches(image, desc1[0], image_2, desc2[0], bf_matches, None)
-cv2.namedWindow('bf_match')
-cv2.imshow('bf_match', image_match_bf)
-cv2.waitKey(0)
+    image_bin_contours = cv2.findContours(image=image_bin, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
 
-# implementation for matches_2 :
-img_pt1 = []
-img_pt2 = []
-for x in matches_2:
-    img_pt1.append(kp1[x.queryIdx].pt)
-    img_pt2.append(kp2[x.trainIdx].pt)
-img_pt1 = np.array(img_pt1)
-img_pt2 = np.array(img_pt2)
+    cnt = image_bin_contours[0]
+    x, y, w, h = cv2.boundingRect(cnt)
 
-# homography :
-M, mask = cv2.findHomography(img_pt2, img_pt1, cv2.RANSAC)
+    crop = final_image[y:y + h, x:x + w]
 
-# merged image :
-merged_image = cv2.warpPerspective(image_2, M, (image.shape[1]+1000, image.shape[0]+1000))
+    cv2.namedWindow('crop')
+    crop_r = cv2.resize(crop, (1300, 800))
+    cv2.imshow('crop', crop_r)
+    cv2.waitKey(0)
 
-cv2.namedWindow('merged_image')
-cv2.imshow('merged_image', merged_image)
-cv2.waitKey(0)
+    return crop
 
-merged_image[0: image.shape[0], 0: image.shape[1]] = image
 
-cv2.namedWindow('merged_image_2')
-cv2.imshow('merged_image_2', merged_image)
-cv2.waitKey(0)
+first_path = 'dataset/yard-house/yard-house-05.png'
+first_image = cv2.imread(first_path)
+first_element = first_image
 
+images = ['dataset/yard-house/yard-house-04.png', 'dataset/yard-house/yard-house-03.png',
+          'dataset/yard-house/yard-house-02.png','dataset/yard-house/yard-house-01.png']
+
+for image_path in images:
+    image = cv2.imread(image_path)
+
+    cv2.namedWindow('image for ' + image_path[8:-4])
+    image_r = cv2.resize(image, (1300, 800))
+    cv2.imshow('image for ' + image_path[8:-4], image_r)
+    cv2.waitKey(0)
+
+    final_image = panorama(first_element, image)
+
+    cv2.namedWindow('final_image for ' + image_path[8:-4])
+    final_image_r = cv2.resize(final_image, (1300, 800))
+    cv2.imshow('final_image for ' + image_path[8:-4], final_image_r)
+    cv2.waitKey(0)
+
+    image_cropped = cropping(final_image)
+
+    cv2.namedWindow('image_cropped for ' + image_path[8:-4])
+    image_cropped_r = cv2.resize(image_cropped, (1300, 800))
+    cv2.imshow('image_cropped for ' + image_path[8:-4], image_cropped_r)
+    cv2.waitKey(0)
+
+    first_element = image_cropped
+    print("first_element changed !")
